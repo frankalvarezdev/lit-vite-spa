f59f2e4e56cc2d8822c638ff568b09876b731ad0
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-27 13:55:45 -0500

    use store in home page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,15 @@
-import { AppElement, html, customElement } from "utils/element";
+import { AppElement, html, customElement, property } from "utils/element";
+import { store } from "utils/store";
 
 @customElement('home-page')
 class Home extends AppElement {
+
+    @property() name: string = 'World';
+
     render() {
-        return html`<h1>Hello world</h1>`;
+        const name = store.get('page').query.name; 
+        this.name = typeof name !== 'undefined' ? name : this.name;
+
+        return html`<h1>Hello ${this.name}</h1>`;
     }
 }
